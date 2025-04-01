cc3cb54d5c1f06b27ce6c5e5b6ddec808fd5b511
DouaeBakkali269 <P138324714@um5.ac.ma>
2025-04-02 01:32:27 +0200

    Expected :1, Actual :null because the method fizzBuzz() returns null (not implemented yet)

--- src/test/java/FizzBuzzTest.java
+++ src/test/java/FizzBuzzTest.java
@@ -1,12 +1,26 @@
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.fail;
 
 class FizzBuzzTest {
 
+//    @Test
+//    void test() {
+//        fail("Not yet implemented");
+//    }
+
     @Test
-    void test() {
-        fail("Not yet implemented");
+    public final void testAvecValeurUn() {
+
+        // Arrange (Setup input) - Input is 1
+        // Act (Call the method)
+        String res = FizzBuzz.fizzBuzz(1);
+
+        // Assert (Check the result)
+        assertEquals("1", res, "Problem with the number 1");
     }
 
 }
+
+// Expected :1, Actual :null because the method fizzBuzz() returns null (not implemented yet)
