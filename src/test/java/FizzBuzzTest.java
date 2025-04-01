249200ad091de29434566d01ba78967a3a344380
DouaeBakkali269 <P138324714@um5.ac.ma>
2025-04-02 01:26:29 +0200

    Test fails not implemented

---
+++ src/test/java/FizzBuzzTest.java
@@ -0,0 +1,12 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
+class FizzBuzzTest {
+
+    @Test
+    void test() {
+        fail("Not yet implemented");
+    }
+
+}
